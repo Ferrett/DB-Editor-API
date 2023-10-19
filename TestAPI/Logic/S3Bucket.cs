294247b86bf39cbf71fb710fb42af9e10305c0c0
Prikhod322 <vovkaprikhod@gmail.com>
2023-10-19 21:09:07 +0300

    small s3bucket fix

--- TestAPI/Logic/S3Bucket.cs
+++ TestAPI/Logic/S3Bucket.cs
@@ -6,19 +6,19 @@ namespace WebAPI.Logic
 {
     public static class S3Bucket
     {
-        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUNorth1;
-
         private static IAmazonS3 client = new AmazonS3Client(bucketRegion);
 
-        public static string DeveloperBucketUrl = @"https://webapilogos.s3.eu-north-1.amazonaws.com/developer/";
-        public static string GameBucketUrl = @"https://webapilogos.s3.eu-north-1.amazonaws.com/game/";
-        public static string UserBucketUrl = @"https://webapilogos.s3.eu-north-1.amazonaws.com/user/";
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUNorth1;
 
-        public static string DeveloperBucketPath = @"webapilogos/developer";
-        public static string GameBucketPath = @"webapilogos/game";
-        public static string UserBucketPath = @"webapilogos/user";
+        public const string DeveloperBucketUrl = @"https://webapilogos.s3.eu-north-1.amazonaws.com/developer/";
+        public const string GameBucketUrl = @"https://webapilogos.s3.eu-north-1.amazonaws.com/game/";
+        public const string UserBucketUrl = @"https://webapilogos.s3.eu-north-1.amazonaws.com/user/";
 
-        public static string DefaultLogoName = "dummy.png";
+        public const string DeveloperBucketPath = @"webapilogos/developer";
+        public const string GameBucketPath = @"webapilogos/game";
+        public const string UserBucketPath = @"webapilogos/user";
+
+        public const string DefaultLogoName = "dummy.png";
 
         public static async Task AddObject(IFormFile file, string bucket, Guid guid)
         {
